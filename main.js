536320c60ed6ac7d331cbece614b116a35bc6aac
Demo17-png <demolaorolu@yahoo.com>
2026-07-01 14:51:29 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+
+document.getElementById("orderForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let name=document.getElementById("name").value;
+let phone=document.getElementById("phone").value;
+let food=document.getElementById("food").value;
+let time=document.getElementById("time").value;
+let comments=document.getElementById("comments").value;
+
+let message=
+`*NEW FOOD ORDER*%0A%0A`+
+`👤 *Full Name:* ${name}%0A`+
+`📱 *WhatsApp Number:* ${phone}%0A`+
+`🍛 *Food Ordered:* ${food}%0A`+
+`⏰ *Delivery Time:* ${time}%0A`+
+`📝 *Comments:* ${comments}`;
+
+let whatsappNumber="2348137846411";
+
+// Replace with your own WhatsApp number
+
+let url=`https://wa.me/${whatsappNumber}?text=${message}`;
+
+window.open(url,"_blank");
+
+});
